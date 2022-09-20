1e6ad00ebd4b08604591f017eb7d330468eb9e03
Daniel Gatto <d.mario.gatto@gmail.com>
2022-09-21 09:05:52 +0930

    Added version info

--- lib/features/stores/screens/store_page.dart
+++ lib/features/stores/screens/store_page.dart
@@ -1,6 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:pondrop/features/app/widgets/loading_overlay.dart';
 import 'package:pondrop/features/authentication/bloc/authentication_bloc.dart';
 import 'package:pondrop/features/stores/widgets/store_list.dart';
@@ -40,80 +43,100 @@ class StorePage extends StatelessWidget {
             ],
             centerTitle: true),
         drawer: Drawer(
-          child: ListView(
-            children: [
-              ListTile(
-                leading: SvgPicture.asset(
-                  'assets/pondrop.svg',
-                  height: 24,
-                ),
-                dense: true,
+            child: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                children: [
+                  ListTile(
+                    leading: SvgPicture.asset(
+                      'assets/pondrop.svg',
+                      height: 24,
+                    ),
+                    dense: true,
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.account_circle_outlined),
+                    minLeadingWidth: 0,
+                    title: Text(email,
+                        style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                            fontSize: 12, fontWeight: FontWeight.w400)),
+                    dense: true,
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.storefront),
+                    title: Text(l10n.shopping),
+                    selected: true,
+                    selectedColor: Colors.black,
+                    selectedTileColor: PondropColors.selectedListItemColor,
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.local_offer_outlined),
+                    title: Text(l10n.products),
+                    selectedColor: Colors.black,
+                    iconColor: Colors.black,
+                    selectedTileColor: PondropColors.selectedListItemColor,
+                    onTap: () {
+                      Navigator.of(context).push(SearchProductPage.route());
+                    },
+                  ),
+                  ListTile(
+                    leading: SvgPicture.asset('assets/barcode_scanner.svg'),
+                    title: Text(l10n.barcodeScan),
+                    selectedColor: Colors.black,
+                    iconColor: Colors.black,
+                    selectedTileColor: PondropColors.selectedListItemColor,
+                    onTap: () {
+                      Navigator.of(context)
+                          .push(BarcodeScannerPage.route(false));
+                    },
+                  ),
+                  BlocListener<AuthenticationBloc, AuthenticationState>(
+                    listenWhen: (previous, current) =>
+                        previous.isLoggingOut != current.isLoggingOut,
+                    listener: (context, state) {
+                      if (state.isLoggingOut) {
+                        LoadingOverlay.of(context)
+                            .show(l10n.itemEllipsis(l10n.loggingOut));
+                      } else {
+                        LoadingOverlay.of(context).hide();
+                      }
+                    },
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.logout,
+                        color: Theme.of(context).errorColor,
+                      ),
+                      title: Text(
+                        l10n.logOut,
+                        style: TextStyle(color: Theme.of(context).errorColor),
+                      ),
+                      onTap: () => context
+                          .read<AuthenticationBloc>()
+                          .add(AuthenticationLogoutRequested()),
+                    ),
+                  ),
+                ],
               ),
-              ListTile(
-                leading: const Icon(Icons.account_circle_outlined),
-                minLeadingWidth: 0,
-                title: Text(email,
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyText1!
-                        .copyWith(fontSize: 12, fontWeight: FontWeight.w400)),
-                dense: true,
-              ),
-              ListTile(
-                leading: const Icon(Icons.storefront),
-                title: Text(l10n.shopping),
-                selected: true,
-                selectedColor: Colors.black,
-                selectedTileColor: PondropColors.selectedListItemColor,
-              ),
-              ListTile(
-                leading: const Icon(Icons.local_offer_outlined),
-                title: Text(l10n.products),
-                selectedColor: Colors.black,
-                iconColor: Colors.black,
-                selectedTileColor: PondropColors.selectedListItemColor,
-                onTap: () {
-                  Navigator.of(context).push(SearchProductPage.route());
-                },
-              ),
-              ListTile(
-                leading: SvgPicture.asset('assets/barcode_scanner.svg'),
-                title: Text(l10n.barcodeScan),
-                selectedColor: Colors.black,
-                iconColor: Colors.black,
-                selectedTileColor: PondropColors.selectedListItemColor,
-                onTap: () {
-                  Navigator.of(context).push(BarcodeScannerPage.route(false));
-                },
-              ),
-              BlocListener<AuthenticationBloc, AuthenticationState>(
-                listenWhen: (previous, current) =>
-                    previous.isLoggingOut != current.isLoggingOut,
-                listener: (context, state) {
-                  if (state.isLoggingOut) {
-                    LoadingOverlay.of(context)
-                        .show(l10n.itemEllipsis(l10n.loggingOut));
-                  } else {
-                    LoadingOverlay.of(context).hide();
+            ),
+            FutureBuilder(
+                future: PackageInfo.fromPlatform(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final packageInfo = snapshot.data as PackageInfo;
+                    return Padding(
+                      padding: const EdgeInsets.fromLTRB(0, Dims.small, 0, Dims.xxLarge),
+                      child: Text(
+                        '${packageInfo.version} (${packageInfo.buildNumber})',
+                        style: Theme.of(context).textTheme.caption!.copyWith(fontSize: 11),
+                      ),
+                    );
                   }
-                },
-                child: ListTile(
-                  leading: Icon(
-                    Icons.logout,
-                    color: Theme.of(context).errorColor,
-                  ),
-                  title: Text(
-                    l10n.logOut,
-                    style: TextStyle(color: Theme.of(context).errorColor),
-                  ),
-                  onTap: () => context
-                      .read<AuthenticationBloc>()
-                      .add(AuthenticationLogoutRequested()),
-                ),
-              ),
-            ],
-          ),
-        ),
+
+                  return const SizedBox.shrink();
+                })
+          ],
+        )),
         body: BlocProvider(
           create: (_) => StoreBloc(
             storeRepository: RepositoryProvider.of<StoreRepository>(context),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,11 +8,13 @@ import Foundation
 import flutter_secure_storage_macos
 import geolocator_apple
 import mobile_scanner
+import package_info_plus_macos
 import shared_preferences_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
